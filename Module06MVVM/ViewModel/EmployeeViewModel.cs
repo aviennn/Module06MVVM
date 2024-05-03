1ccae7e5af31b429c1b72233df6b24ea2d6d80c3
User <User@B08>
2024-05-03 15:36:23 +0800

    Add project files.

---
+++ Module06MVVM/ViewModel/EmployeeViewModel.cs
@@ -0,0 +1,51 @@
+﻿using Module06MVVM.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+
+namespace Module06MVVM.ViewModel
+{
+    public class EmployeeViewModel
+    {
+
+        /*public EmployeeModel EmployeeModelSet { get; set; }
+
+        public EmployeeViewModel()
+        {
+            EmployeeModelSet = new EmployeeModel
+            {
+                Id = 1,
+                Name = "Juan Dela Cruz",
+                Email = "juandelacruz@auf.edu.ph",
+                Address = "Angeles City"
+            };
+        }*/
+
+
+        private Services.DatabaseContext getContext() 
+        {
+            return new Services.DatabaseContext();  
+        }
+
+        public int InsertEmployee(EmployeeModel obj)
+        {
+            var _dbContext = getContext();
+            _dbContext.Employees.Add(obj);
+            int c = _dbContext.SaveChanges();
+            return c;
+        }
+
+        public async Task<List<EmployeeModel>> GetAllEmployees()
+        {
+            var _dbContext = getContext();
+            var res = await _dbContext.Employees.ToListAsync();
+            return res;
+        }
+    }
+}
